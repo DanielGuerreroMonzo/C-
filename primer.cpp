52c88c1597ce12115c9fa8d5967d8c4f71a781f4
Daniel Guerrero <113890266+DanielGuerreroMonzo@users.noreply.github.com>
2022-12-19 10:07:23 +0100

    Update primer.cpp

---
+++ primer.cpp
@@ -0,0 +1,9 @@
+int main(){
+    //definimos 3 variables
+    int x,y,z;
+    cout<<"introduce tres variables: ";
+    cin>> x >> y >> z;
+    cout<<"Has introducido las variables: ";
+    cout<< x <<"," <<y <<"," << z << endl;
+    return 0;
+}
